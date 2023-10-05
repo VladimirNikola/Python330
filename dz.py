8ff3de1c4e51993f3d3401b17fb2009fe2ac2aeb
VladimirNikola <sobali01@mail.ru>
2023-10-05 17:51:45 +0300

    added print

--- dz.py
+++ dz.py
@@ -1,17 +1,26 @@
 # dz13
-def func(a):
-    numb = ''
-    while a > 0:
-        numb = str(a % 2) + numb
-        a = a // 2
-    print(numb)
+# def func(a):
+#     numb = ''
+#     while a > 0:
+#         numb = str(a % 2) + numb
+#         a = a // 2
+#     print(numb)
+#
+#
+# while True:
+#     x = int(input('-> '))
+#     if x == 0:
+#         break
+#     func(x)
 
-
-while True:
-    x = int(input('-> '))
-    if x == 0:
-        break
-    func(x)
-
-
+#dz14
+s = """Ежевику для ежат
+Принесли два ежа.
+Ежевику еле-еле
+Ежата возле ели съели. """
+print(s)
+up_e = s.count('Е')
+lower_e = s.count('е')
+count = up_e + lower_e
+print('кол-во слов:', count)
 
