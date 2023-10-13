f0af3c2aa0d3e022ba1476e122da258082596ff5
VladimirNikola <sobali01@mail.ru>
2023-10-13 10:26:29 +0300

    added print

--- dz.py
+++ dz.py
@@ -27,8 +27,46 @@
 
 # dz 15
 
-import re
+# import re
+#
+# s = '+7 499 456-45-78 , +74994564578 , 7 (499) 456 45 78 , 7 (499) 456-45-78'
+# reg = r'\+?7\s*\(?\d+\)?\s*\d+[- ]?\d+[- ]?\d+'
+# print(re.findall(reg, s))
 
-s = '+7 499 456-45-78 , +74994564578 , 7 (499) 456 45 78 , 7 (499) 456-45-78'
-reg = r'\+?7\s*\(?\d+\)?\s*\d+[- ]?\d+[- ]?\d+'
-print(re.findall(reg, s))
+# dz 16
+
+arr = ['Замена строки в текстовом файле;', 'изменить строку в списке;', 'записать список в файл;']
+
+with open('text.txt', 'w') as text:
+    for i in arr:
+        text.write(i + '\n')
+
+full_text = []
+
+
+def read_text():
+    global full_text
+    with open ('text.txt', 'r') as text_:
+        full_text = text_.readlines()
+        for j in full_text:
+            print(j, end='')
+
+
+read_text()
+
+pos1 = int(input('\n' + 'pos1 = ')) - 1
+pos2 = int(input('pos2 = ')) - 1
+
+print()
+
+if 0 <= pos1 <= pos2 <= len(full_text):
+    full_text[pos1], full_text[pos2] = full_text[pos2], full_text[pos1]
+else:
+    print("Индекс введен не верно!")
+
+with open('text.txt', 'w') as text:
+    for i in full_text:
+        text.writelines(i)
+
+read_text()
+
