e24e45f2864cee3acf9966ec0bbd421e4e61a24f
VladimirNikola <sobali01@mail.ru>
2023-12-24 20:33:36 +0300

    added print

--- main.py
+++ main.py
@@ -7964,3 +7964,64 @@ with sqlite3.connect("top-academy.db") as con:
 # ORM
 
 # pip install sqlalchemy
+
+
+import os
+
+from models.database import DATABASE_NAME, Session
+import create_database as db_creator
+from models.lesson import Lesson, association_table
+from models.student import Student
+from models.group import Group
+from sqlalchemy import and_, or_, not_
+
+if __name__ == '__main__':
+    db_is_created = os.path.exists(DATABASE_NAME)
+    if not db_is_created:
+        db_creator.create_database()
+
+    session = Session()
+    print(session.query(Lesson).all())
+    print("*" * 60)
+
+    for it in session.query(Lesson):
+        print(it.lesson_title)
+    print("*" * 60)
+
+    print(session.query(Lesson).count())
+    print("*" * 60)
+
+    print(session.query(Lesson).first())
+    print("*" * 60)
+
+    for it in session.query(Lesson).filter(Lesson.id > 3):
+        print(it)
+    print("*" * 60)
+
+    for it in session.query(Lesson).filter(or_(Lesson.id >= 3, Lesson.lesson_title.like('М%'))):
+        print(it)
+    print("*" * 60)
+
+    for it in session.query(Lesson).filter(not_(Lesson.id >= 3), not_(Lesson.lesson_title.like('М%'))):
+        print(it)
+    print("*" * 60)
+
+    for it, gr in session.query(Lesson.lesson_title, Group.group_name).filter(and_(association_table.c.lesson_id == Lesson.id, association_table.c.group_id == Group.id, Group.group_name == 'MDA-9')):
+        print(it, gr)
+    print("*" * 60)
+
+    print(session.query(Lesson).filter(Lesson.lesson_title is not None).all())
+    print("*" * 60)
+
+    print(session.query(Lesson).filter(Lesson.lesson_title.in_(['Математика', 'Линейная алгебра'])).all())
+    print("*" * 60)
+
+    print(session.query(Lesson).filter(Lesson.lesson_title.notin_(['Математика', 'Линейная алгебра'])).all())
+    print("*" * 60)
+
+    print(session.query(Student).filter(not_(Student.age.between(16, 17))).all())
+    print("*" * 60)
+
+    print(session.query(Student).filter(Student.age.like("1%")).limit(4).offset(3).all())
+    print("*" * 60)
+
--- models/student.py
+++ models/student.py
@@ -11,3 +11,15 @@ class Student(Base):
     patronymic = Column(String(250), nullable=False)
     age = Column(Integer)
     group = Column(Integer, ForeignKey('groups.id'))
+
+    def __init__(self, full_name, age, id_group):
+        self.surname = full_name[0]
+        self.name = full_name[1]
+        self.patronymic = full_name[2]
+        self.age = age
+        self.group = id_group
+
+    def __repr__(self):
+        return f"Студент (ФИО: {self.surname} {self.name} {self.patronymic}, " \
+               f"Возраст: {self.age}, ID группы: {self.group})"
+
