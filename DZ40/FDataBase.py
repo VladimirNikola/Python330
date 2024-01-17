6c88e6f00edd5df3c47d7e275dacc7ad8edbdba5
VladimirNikola <sobali01@mail.ru>
2024-01-17 16:37:52 +0300

    added print

--- DZ40/FDataBase.py
+++ DZ40/FDataBase.py
@@ -1,3 +1,10 @@
+import time
+import math
+import sqlite3
+import re
+from flask import url_for
+
+
 class FDataBase:
     def __init__(self, db):
         self.__db = db
@@ -13,3 +20,46 @@ class FDataBase:
         except IOError:
             print("Ошибка чтения из БД")
         return []
+
+    def add_post(self, title, price, text, url):
+        try:
+            self.__cur.execute("SELECT COUNT() as 'count' FROM posts WHERE url LIKE ?", (url,))
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("Статья с таким url уже существует")
+                return False
+
+            base = url_for('static', filename='images')
+            text = re.sub(r"(?P<tag><img\s+[^>]*src=)(?P<quote>[\"'])(?P<url>.+?)(?P=quote)>",
+                          r"\g<tag>" + base + r"/\g<url>>", text)
+
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?, ?, ?)", (title, price, text, url, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления курса в БД:", e)
+            return False
+
+        return True
+
+    def get_posts_anonce(self):
+        try:
+            self.__cur.execute("SELECT id, title, price, text, url FROM posts ORDER BY time DESC")
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка получения курса из БД:", e)
+
+        return []
+
+    def get_info(self, alias):
+        try:
+            self.__cur.execute(f"SELECT title, price, text FROM posts WHERE url LIKE '{alias}'")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка получения курса из БД:", e)
+
+        return False, False, False
--- DZ40/main.py
+++ DZ40/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, g
+from flask import Flask, render_template, request, flash, g, abort
 import sqlite3
 import os
 from FDataBase import FDataBase
@@ -27,13 +27,6 @@ def create_db():
     db.close()
 
 
-# menu = [
-#     {"name": "Главная", "url": "index"},
-#     {"name": "О компании", "url": "about"},
-#     {"name": "Контакты", "url": "contact"}
-# ]
-
-
 def get_db():
     if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
@@ -47,34 +40,39 @@ def close_db(error):
 
 
 @app.route("/")
-# @app.route("/index", methods=["POST", "GET"])
 def index():
     db = get_db()
     dbase = FDataBase(db)
-    return render_template('index.html', title="Купленные билеты", menu=dbase.get_menu())
+    return render_template('index.html', title="Каталог курсов", menu=dbase.get_menu(), posts=dbase.get_posts_anonce())
 
 
 @app.route("/add_post", methods=["POST", "GET"])
 def add_post():
     db = get_db()
     dbase = FDataBase(db)
-    if request.method == 'POST':
-        if len(request.form['surname']) > 2:
-            flash("Сообщение отправлено успешно!", category="success")
+
+    if request.method == "POST":
+        if len(request.form['name']) > 10 and len(request.form['price']) > 10 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['price'], request.form['post'], request.form['url'])
+            if not res:
+                flash("Ошибка добавления курса", category="error")
+            else:
+                flash("Курс добавлен успешно", category="success")
         else:
-            flash("Ошибка отправки", category="error")
+            flash("Ошибка добавления курса", category="error")
 
-    return render_template('add_post.html', title="Купить билеты", menu=dbase.get_menu())
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title="О компании", menu=[])
+    return render_template('add_post.html', title="Добавление курса", menu=dbase.get_menu())
 
 
-@app.route("/contact")
-def contact():
-    return render_template('contact.html', title="Контакты", menu=[])
+@app.route("/info/<alias>")
+def add_info(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, price, text = dbase.get_info(alias)
+    if not title:
+        abort(404)
+
+    return render_template('info.html', menu=dbase.get_menu(), title=title, price=price, text=text)
 
 
 @app.errorhandler(404)
